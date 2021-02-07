5149bf7f7432512f28edbd1dd22f9eddf4b7ec72
Durgesh-zs <durgeshbabal@gmail.com>
2021-02-07 20:18:30 +0530

    Create message.go

---
+++ models/message.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type Message struct {
+	Operation string `json:"operation"`
+	Value     string `json:"value"`
+	Target    string `json:"target"`
+}
+
+func (m *Message) String() string {
+	return string(m.Bytes())
+}
+
+func (m *Message) Bytes() []byte {
+	s, err := json.Marshal(m)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return s
+}
